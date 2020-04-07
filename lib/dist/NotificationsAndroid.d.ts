ceb4cd50815ed5b706c6cabd423987df9675c830
Bogdan Preda <bogdan@creditstacks.com>
2020-04-07 12:02:10 +0300

    added dist

---
+++ lib/dist/NotificationsAndroid.d.ts
@@ -0,0 +1,10 @@
+import 'proxy-polyfill';
+import { Commands } from './commands/Commands';
+export declare class NotificationsAndroid {
+    private readonly commands;
+    constructor(commands: Commands);
+    /**
+    * Refresh FCM token
+    */
+    registerRemoteNotifications(): void;
+}
